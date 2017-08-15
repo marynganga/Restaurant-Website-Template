556b96fbaf585f68f1c453f249f8dc43b7888e7f
marynganga <marynganga.mw@gmail.com>
2017-08-15 23:11:41 +0300

    Section 5- Style the about menu and testimonial section. Section 6- Add content including form and  bg-image. Section 7- Add content including images and bg-color.

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+//scripts for the testimonial carousel
+$('document').ready(function() {
+      document.getElementsByTagName(input[type = button]) {
+
+      }
+      /*$('#testimonials').carousel({
+        interval: 10000
+      })
+
+      $('.carousel .item').each(function() {
+        var next = $(this).next();
+        if (!next.length) {
+          next = $(this).siblings(':first');
+        }
+        next.children(':first-child').clone().appendTo($(this));
+
+        if (next.next().length > 0) {
+          next.next().children(':first-child').clone().appendTo($(this));
+        } else {
+          $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+        }
+      });
